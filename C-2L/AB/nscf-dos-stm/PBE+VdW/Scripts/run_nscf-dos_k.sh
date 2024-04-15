e833da1eb81e18d7fb43b62d0533684cd5ef9bd6
Maurizia Palummo <maurizia.palummo@gmail.com>
2024-04-15 21:33:18 +0200

    nscf-dos-stm

--- C-2L/AB/nscf-dos-stm/PBE+VdW/Scripts/run_nscf-dos_k.sh
+++ C-2L/AB/nscf-dos-stm/PBE+VdW/Scripts/run_nscf-dos_k.sh
@@ -1,12 +1,13 @@
 #!/bin/zsh
 
-for kpt in  40 50 60 
+mpirun pw.x < c_2l_AB.scf.in > c_2l_AB.scf.out
+for kpt in  20 40 50 60 
 do
 
 echo "starting nscf calculation for kpt = $kpt"
 
 cp -r tmp tmp$kpt
-sed "s/12 12/$kpt $kpt/" c_1l.nscf.in > kpttmp_$kpt.in
+sed "s/12 12/$kpt $kpt/" c_2l_AB.nscf.in > kpttmp_$kpt.in
 sed "s/tmp/tmp$kpt/"  kpttmp_$kpt.in > kpt_$kpt.in
 sed "s/tmp/tmp$kpt/"  dos.in > dostmp_$kpt.in
 sed "s/graphene_PBE_vdW_DOS/graphene_PBE_vdW_DOS$kpt/"  dostmp_$kpt.in > dos_$kpt.in
