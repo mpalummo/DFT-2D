ef01a7a50b5e8d1f3c74d6fed6060c5cb7b5ad1e
Maurizia Palummo <maurizia.palummo@gmail.com>
2024-04-15 13:06:28 +0200

    C-2L

--- C-2L/AA/nscf-dos-stm/PBE+VdW/run_nscf-dos_stm.sh
+++ C-2L/AA/nscf-dos-stm/PBE+VdW/run_nscf-dos_stm.sh
@@ -7,7 +7,7 @@ echo "starting nscf calculation for kpt = $kpt"
 
 mpirun pw.x < c_2l_AA.scf.in > c_2l_AA.scf.out
 mpirun pw.x < c_2l_AA.nscf.in > c_2l_AA.nscf.out
-mpirun dos.x < dos.in > dos.out
+#mpirun dos.x < dos.in > dos.out
 mpirun pp.x < stm.in > stm.out
 
 echo "calculation for kpt = $kpt completed"
