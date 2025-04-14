1917d3562598df4234e021c117467e111da97d1e
Maurizia Palummo <maurizia.palummo@gmail.com>
2025-04-14 06:16:12 +0200

    c-2l

--- C-2L/AB/nscf-eps/PBE+vdW/Scripts/run_nscf-eps.sh
+++ C-2L/AB/nscf-eps/PBE+vdW/Scripts/run_nscf-eps.sh
@@ -1,17 +1,19 @@
 #!/bin/zsh
 
-mpirun pw.x < c_2l_AB.scf.in > c_2l_AB.scf.out
-for i in 3; do 
+#mpirun pw.x < c_2l_AB.scf.in > c_2l_AB.scf.out
+for i in 16; do 
 kpt=$i
-echo "starting nscf calculation for kpt = $kpt"
 cp -r tmp tmp_$kpt
 sed "s/tmp/tmp_${kpt}/" c_2l_AB.nscf.in	 > aa 
 sed "s/16 16 1 0 0 0/$kpt $kpt 1 0 0 0/" aa > c_2l_AB.nscf_${kpt}.in	 
-sed "s/tmp/tmp_${kpt}/" epsilon.in > epsilon_$kpt.in 
-
+sed "s/tmp/tmp_${kpt}/" epsilon1.in > epsilon1_$kpt.in
+sed "s/tmp/tmp_${kpt}/" epsilon2.in > epsilon2_$kpt.in 
+echo "starting nscf calculation for kpt = $kpt"
 mpirun pw.x < c_2l_AB.nscf_${kpt}.in > c_2l_AB.nscf_${kpt}.out
-mpirun epsilon.x < epsilon_${kpt}.in> epsilon_${kpt}.out
+echo "starting eps calculation for kpt = $kpt"
+mpirun epsilon.x < epsilon1_${kpt}.in> epsilon1_${kpt}.out
+mpirun epsilon.x < epsilon2_${kpt}.in> epsilon2_${kpt}.out
 echo "calculation for kpt = $kpt completed"
-!rm -rf aa
+rm -rf aa
 done
 
