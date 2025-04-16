02cd4ff295b82ee2a1900a25d1a2abec2c23967c
Maurizia Palummo <maurizia.palummo@gmail.com>
2025-04-16 09:47:21 +0200

    run

---
+++ C-1L/convergence/PBE/Vacuum/run_vacuum.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+for i in 4 5 6 7 8 
+do
+
+cellz=$i
+c=$((4.635*i))
+echo ${c}
+echo "starting calculation for celldm(3) = $cellz"
+
+sed "s/celldm(3) = 6/celldm(3) = $cellz/" c_1l.scf.in > c_1l.scf_$c.in
+
+mpirun pw.x < c_1l.scf_$c.in > c_1l.scf_$c.out
+mpirun pp.x < pp.in > pp.out
+average.x < average.in > average.out
+
+rm -fr tmp
+
+echo "calculation for celldm(3) = $cellz completed"
+mv avg.dat avg.dat_$c
+done
+
+grep '!  ' *.out >> etotv_tmp.dat
+sed -i '' -e "s/c_1l.scf_//" etotv_tmp.dat
+sed -i '' -e "s/.out:!    total energy              =//" etotv_tmp.dat
+
+
+sort -n etotv_tmp.dat > Etot_vs_vac_PBE.dat
+rm etotv_tmp.dat
